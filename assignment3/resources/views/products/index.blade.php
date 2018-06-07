77edcf6ead0a247b02c818f8e7ff346b7939b436
Axel Alfredsson <aalfredsson94@gmail.com>
2018-06-07 10:32:55 +0200

    Add folder products

---
+++ assignment3/resources/views/products/index.blade.php
@@ -0,0 +1,23 @@
+@extends("products.layout")
+
+@section("content")
+
+<h1>Alla produkter</h1>
+
+<ul class="list-group">
+@foreach ($produts as $product)
+    <li class="list-group-item">
+        <a href="/movies/{{ $movie->id }}">{{ $product->title }} ({{ $product->price }})</a>
+        <form action="{{ route('movies.destroy', ['movie' => $movie->id]) }}" method="POST" style="float:right; margin-bottom:-5px; margin-top: -3px;">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Radera" class="btn btn-danger btn-sm">
+        </form>
+        <a href="/products/{{ $product->id }}/edit" class="btn btn-warning btn-sm" style="float:right;  margin: -3px 5px -5px 0;">Redigera</a>
+    </li>
+@endforeach
+</ul>
+<p>Nedan listas alla produkter upp</p>
+@foreach ($products as $product)
+    {{ $product->title }}
+@endforeach
